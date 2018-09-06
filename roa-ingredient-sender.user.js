c4c25f34746b7ca8181be8dffeeb2a448f565ef5
John <batosi05@gmail.com>
2018-09-06 15:36:16 -0400

    .

--- roa-ingredient-sender.user.js
+++ roa-ingredient-sender.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Avabur Ingredient Sender
 // @namespace    some_random_string_alujgfkadsglagfyuifgsidgf3
-// @version      0.0.4
+// @version      0.0.5
 // @description  In game ui to build a iwire list
 // @author       Batosi
 // @match        https://*.avabur.com/game*
